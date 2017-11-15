c01d43bc3706dd659aac6aec4ca2c8a9987430eb
Rodrigo Junger de Carvalho <rodrigojunger@id.uff.br>
2017-11-15 13:22:04 -0200

    Fix spam when cert was not found, add dev_hdd0/disc to game search, improved prints a little bit

--- rpcstools/rpcstools.py
+++ rpcstools/rpcstools.py
@@ -5,6 +5,8 @@ import yaml
 import urllib3
 import tqdm
 import xml.etree.ElementTree as ET
+import itertools
+
 from .sfo import SfoFile
 
 
@@ -34,22 +36,24 @@ def get_rpcs3_dir():
     return None
 
 
-def get_title_id(paramsfo_location):
-    with open(paramsfo_location, "rb") as f:
-        the_sfo = SfoFile.from_reader(f)
-        return the_sfo["TITLE_ID"]
+def get_title_id(game_folder):
+    try:
+        with open(os.path.join(game_folder, "PARAM.SFO"), "rb") as f:
+            the_sfo = SfoFile.from_reader(f)
+            return the_sfo["TITLE_ID"]
+    except FileNotFoundError:
+        try:
+            with open(os.path.join(game_folder, "PS3_GAME", "PARAM.SFO"), "rb") as f:
+                the_sfo = SfoFile.from_reader(f)
+                return the_sfo["TITLE_ID"]
+        except FileNotFoundError:
+            return None
 
 
-def download_updates(tid, base_dir):
-    content_folder = os.path.join(base_dir, 'game_updates', str(tid))
-
-    cert_path = os.path.join(base_dir, "dev_flash", "data", "cert", "CA05.cer")
-    if not os.path.isfile(cert_path):
-        cert_path = False
-        print("Couldn't find certificates on RPCS3 folder, going to ignore SSL."
-              "To fix this just follow the rpcs3 quickstart guide")
-
+def download_updates(tid, base_dir, cert_path):
     print("Downloading updates for title_id {}".format(tid))
+
+    content_folder = os.path.join(base_dir, 'game_updates', str(tid))
 
     if not os.path.isdir(content_folder):
         os.mkdir(content_folder)
@@ -59,14 +63,14 @@ def download_updates(tid, base_dir):
     try:
         xml_tree = ET.fromstring(r.text)
     except ET.ParseError:
-        print("Failed to parse xml for {} (Game might not have any updates)".format(tid))
+        print("Failed to parse xml for {} (Game might not have any updates)\n".format(tid))  # Extra \n is intentional
         return
 
     for node in xml_tree.iter('package'):
         disk_filename = node.attrib['url'].split(os.path.sep)[-1]
         disk_filepath = os.path.join(content_folder, disk_filename)
 
-        if not os.path.isfile(disk_filepath) or os.path.getsize(disk_filepath) != node.attrib['size']:
+        if not os.path.isfile(disk_filepath) or os.path.getsize(disk_filepath) != int(node.attrib['size']):
             r = requests.get(node.attrib['url'],
                              verify=cert_path,
                              stream=True)
@@ -84,11 +88,13 @@ def download_updates(tid, base_dir):
                     f.write(data)
                     pbar.update(1024)
                 pbar.close()
+    print()
 
 
 # TODO: Argument for the rpcs3 folder
 # TODO: Handle more exceptions/possible error cases
 # TODO: Better/More organized printing of information
+# TODO: Variable naming is hard
 def update_games():
     # Silence warnings caused by HIGH QUALITY Sony certs
     urllib3.disable_warnings(urllib3.exceptions.SubjectAltNameWarning)
@@ -99,14 +105,18 @@ def update_games():
         raise FileNotFoundError("Couldn't find RPCS3 folder, make sure you call "
                                 "update-rpcs3-games from the rpcs3 folder if you're not on Linux.")
 
-    games_dir = os.path.join(base_dir, "dev_hdd0", "game")
+    games_dirs = (os.path.join(base_dir, "dev_hdd0", "game"),
+                  os.path.join(base_dir, "dev_hdd0", "disc"))
+
     game_ids = []
 
-    for game_dir in os.listdir(games_dir):
-        try:
-            game_ids.append(get_title_id(os.path.join(games_dir, game_dir, "PARAM.SFO")))
-        except FileNotFoundError as e:
-            print("warning: File \"{}\" does not exist and the game wont be updated.".format(e.filename))
+    for games_dir in games_dirs:
+        for game_dir in os.listdir(games_dir):
+            tid = get_title_id(os.path.join(games_dir, game_dir))
+            if tid is not None:
+                game_ids.append(tid)
+            else:
+                print("warning: Couldn't find PARAM.SFO on  \"{}\" ".format(os.path.join(games_dir, game_dir)))
 
     try:
         with open(os.path.join(base_dir, 'games.yml'), "r") as f:
@@ -115,10 +125,11 @@ def update_games():
         games_yml = {}
 
     for key in games_yml.keys():
-        try:
-            game_ids.append(get_title_id(os.path.join(games_yml[key], "PS3_GAME", "PARAM.SFO")))
-        except FileNotFoundError as e:
-            print("warning: File \"{}\" does not exist and the game wont be updated.".format(e.filename))
+        tid = get_title_id(games_yml[key])
+        if tid is not None:
+            game_ids.append(tid)
+        else:
+            print("warning: File \"{}\" does not exist and the game wont be updated.".format(games_yml[key]))
 
     print("Found game ids: {}".format(game_ids))
     print("Starting downloads...")
@@ -128,5 +139,15 @@ def update_games():
     if not os.path.isdir(downloads_path):
         os.mkdir(downloads_path)
 
+    cert_path = os.path.join(base_dir, "dev_flash", "data", "cert", "CA05.cer")
+    if not os.path.isfile(cert_path):
+        cert_path = False
+        print("Couldn't find certificates on RPCS3 folder, going to ignore SSL."
+              "To fix this just follow the rpcs3 quickstart guide")
+
     for title_dir in game_ids:
-        download_updates(title_dir, base_dir)
+        download_updates(title_dir, base_dir, cert_path)
+
+
+if __name__ == "__main__":
+    update_games()
