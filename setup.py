bcd52705c88787257532f17a5e79e9b49e1e797b
Rodrigo Junger de Carvalho <rodrigojunger@id.uff.br>
2017-11-17 00:23:14 -0200

    Fix pip install

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README'), encoding='utf-8') as f:
     long_description = f.read()
 
 
@@ -14,14 +14,15 @@ with open(path.join(here, 'LICENSE'), encoding='utf-8') as f:
 
 setup(
     name='rpcstools',
-    version='0.1.0',
+    version='0.1.2',
     description='Simple tool for rpcs3 that can update games and the emulator itself.',
     long_description=long_description,
     author='Rodrigo Junger',
     author_email='rodrigojunger@id.uff.br',
     url='https://github.com/rodjun/rpcstools',
-    download_url="https://github.com/rodjun/rpcstools/archive/0.1.tar.gz",
+    download_url="https://github.com/rodjun/rpcstools/archive/0.1.1.tar.gz",
     license=license_text,
+    python_requires='>=3',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
